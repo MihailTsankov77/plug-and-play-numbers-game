ad83599250c3b2754fb549db425fa31696c8a639
Mihail Tsankov <mihail.tsankov@trading212.com>
2025-07-01 02:42:05 +0300

    fix bugs

--- src/cell.collection/hooks/useCellState.ts
+++ src/cell.collection/hooks/useCellState.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { CellId, CellCollection } from "../types/Cell";
 import { Direction } from "../types/Dimensions";
 import {
@@ -49,69 +49,80 @@ export function useCellState(): {
   cellCollection: CellCollection;
   addCell: (containerId: string, cellId: CellId, direction: Direction) => void;
 } {
-  const [cellCollection, setCellCollection] = useState<CellCollection>(() =>
+  const [initValue] = useState<CellCollection>(() =>
     generateCellRow([generateCellId()])
   );
+  const cellCollectionRef = useRef<CellCollection>(initValue);
+
+  const [refChange, forceRefChange] = useState({});
 
   const addCell = useCallback(
     (containerId: string, cellId: CellId, _direction: Direction) => {
-      setCellCollection((prev) => {
-        const container = findElementById(prev, containerId);
-        if (!container) {
-          console.error(`Container with id ${containerId} not found`);
-          return prev;
+      const container = findElementById(cellCollectionRef.current, containerId);
+      if (!container) {
+        console.error(`Container with id ${containerId} not found`);
+        return;
+      }
+
+      const newChildren = container.children;
+
+      const index = newChildren.indexOf(cellId);
+      if (index === -1) {
+        console.error(
+          `Cell with id ${cellId} not found in container ${containerId}`,
+          cellCollectionRef.current
+        );
+        return;
+      }
+
+      const direction =
+        container.type === "column"
+          ? TransformDirectionsForColumn[_direction]
+          : _direction;
+
+      const generator =
+        container.type === "column" ? generateCellRow : generateCellColumn;
+
+      switch (direction) {
+        case "up": {
+          newChildren[index] = generator([generateCellId(), cellId]);
+
+          break;
+        }
+        case "down": {
+          newChildren[index] = generator([cellId, generateCellId()]);
+
+          break;
+        }
+        case "left": {
+          newChildren.splice(index, 0, generateCellId());
+          break;
+        }
+        case "right": {
+          newChildren.splice(index + 1, 0, generateCellId());
+          break;
         }
 
-        const newChildren = container.children;
-
-        const index = newChildren.indexOf(cellId);
-        if (index === -1) {
-          console.error(
-            `Cell with id ${cellId} not found in container ${containerId}`,
-            cellCollection
-          );
-          return prev;
+        default: {
+          direction satisfies never;
         }
+      }
 
-        const direction =
-          container.type === "column"
-            ? TransformDirectionsForColumn[_direction]
-            : _direction;
+      container.children = newChildren;
 
-        const generator =
-          container.type === "column" ? generateCellRow : generateCellColumn;
+      cellCollectionRef.current = updateCollection(
+        cellCollectionRef.current,
+        container
+      );
 
-        switch (direction) {
-          case "up": {
-            newChildren[index] = generator([generateCellId(), cellId]);
-
-            break;
-          }
-          case "down": {
-            newChildren[index] = generator([cellId, generateCellId()]);
-
-            break;
-          }
-          case "left": {
-            newChildren.splice(index, 0, generateCellId());
-            break;
-          }
-          case "right": {
-            newChildren.splice(index + 1, 0, generateCellId());
-            break;
-          }
-
-          default: {
-            direction satisfies never;
-          }
-        }
-
-        container.children = newChildren;
-        return updateCollection(prev, container);
-      });
+      forceRefChange({});
     },
     []
   );
+
+  const cellCollection = useMemo(() => {
+    return { ...cellCollectionRef.current };
+  }, [refChange]);
 
   return {
     cellCollection,
