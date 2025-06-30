1c90ae74f850318a95e56e52cfb15059c1bb965b
Mihail Tsankov <mihail.tsankov@trading212.com>
2025-06-30 23:59:17 +0300

    adding cells init

--- src/cell.collection/hooks/useCellState.ts
+++ src/cell.collection/hooks/useCellState.ts
@@ -2,6 +2,7 @@ import { useCallback, useState } from "react";
 import { CellConfig, CellId } from "../types/Cell";
 import { Direction } from "../types/Dimensions";
 import { CellCollection, CellColumn, CellRow } from "../types/CellCollection";
+import { generateEmptyCell } from "../utils/generateCell";
 
 function generateRow(children: (CellId | CellColumn)[]): CellRow {
   return {
@@ -34,6 +35,23 @@ function findElementById(
   }
 }
 
+function updateCollection(
+  collection: CellCollection,
+  updatedElement: CellCollection
+): CellCollection {
+  if (collection.id === updatedElement.id) {
+    return updatedElement;
+  }
+
+  const newChildren = collection.children.map((child) => {
+    if (typeof child === "string") return child;
+    return updateCollection(child, updatedElement);
+  });
+
+  return { ...collection, children: newChildren as any };
+}
+
+// TODO maybe improve this so that columns can accept cells as children it will flatten the structure
 export function useCellState(initStateFn: () => CellConfig): {
   cellCollection: CellCollection;
   cellsById: Record<string, CellConfig>;
@@ -47,64 +65,115 @@ export function useCellState(initStateFn: () => CellConfig): {
     [initCell.id]: initCell,
   });
 
-  const addToRow = useCallback(
-    (row: CellRow, cellId: CellId, direction: Direction) => {
-      setCellCollection((prevCollection) => {
-        switch (direction) {
-          case "up": {
-            break;
-          }
-          case "down": {
-            break;
-          }
-          case "left": {
-            break;
-          }
-          case "right": {
-            break;
-          }
+  const addNewCell = useCallback(() => {
+    const newCell = generateEmptyCell({ x: 0, y: 0 });
 
-          default: {
-            direction satisfies never;
-          }
-        }
+    setCellsById((prev) => ({ ...prev, [newCell.id]: newCell }));
+    return newCell.id;
+  }, []);
 
-        return prevCollection;
-      });
-    },
-    []
-  );
+  const addHorizontalToRow = useCallback(
+    (containerId: string, cellId: CellId, direction: "left" | "right") => {
+      const container = findElementById(cellCollection, containerId);
+      if (!container || container.type !== "row") {
+        console.error(`Container with id ${containerId} not found`);
+        return;
+      }
 
-  const addCell = useCallback(
-    (containerId: string, cellId: CellId, direction: Direction) => {
-      const element = findElementById(cellCollection, containerId);
-
-      if (!element) {
+      const index = container.children.indexOf(cellId);
+      if (index === -1) {
         console.error(
-          `Element with id ${containerId} not found in the collection.`
+          `Cell with id ${cellId} not found in container ${containerId}`,
+          cellCollection
         );
         return;
       }
 
-      switch (element.type) {
-        case "row": {
-          return addToRow(element, cellId, direction);
+      setCellCollection((prev) => {
+        const targetRow = findElementById(prev, containerId) as CellRow;
+        const newChildren = targetRow.children;
+
+        if (direction === "left") {
+          // TODO coordinates
+          newChildren.splice(index, 0, addNewCell());
+        } else {
+          // TODO coordinates
+          newChildren.splice(index + 1, 0, addNewCell());
         }
-        case "column": {
-          console.error("Please use rows.");
+
+        targetRow.children = newChildren;
+        return updateCollection(prev, targetRow);
+      });
+    },
+    [cellCollection]
+  );
+
+  const addVerticalToRow = useCallback(
+    (containerId: string, cellId: CellId, direction: "up" | "down") => {
+      const container = findElementById(cellCollection, containerId);
+      if (!container || container.type !== "row") {
+        console.error(`Container with id ${containerId} not found`);
+        return;
+      }
+
+      const index = container.children.indexOf(cellId);
+      if (index === -1) {
+        console.error(
+          `Cell with id ${cellId} not found in container ${containerId}`,
+          cellCollection
+        );
+        return;
+      }
+
+      setCellCollection((prev) => {
+        const targetRow = findElementById(prev, containerId) as CellRow;
+        const newChildren = targetRow.children;
+
+        if (direction === "up") {
+          // TODO coordinates
+          newChildren[index] = generateColumn([
+            generateRow([addNewCell()]),
+            generateRow([cellId]),
+          ]);
+        } else {
+          newChildren[index] = generateColumn([
+            generateRow([cellId]),
+            generateRow([addNewCell()]),
+          ]);
         }
+
+        targetRow.children = newChildren;
+        return updateCollection(prev, targetRow);
+      });
+    },
+    [cellCollection]
+  );
+
+  const addToRow = useCallback(
+    (containerId: string, cellId: CellId, direction: Direction) => {
+      switch (direction) {
+        case "up":
+        case "down": {
+          addVerticalToRow(containerId, cellId, direction);
+          break;
+        }
+        case "left":
+        case "right": {
+          addHorizontalToRow(containerId, cellId, direction);
+          break;
+        }
+
         default: {
-          console.error("Unknown element type:", element);
-          return;
+          direction satisfies never;
         }
       }
     },
-    [addToRow]
+    []
   );
 
   return {
     cellCollection,
     cellsById,
-    addCell,
+    addCell: addToRow,
   };
 }
